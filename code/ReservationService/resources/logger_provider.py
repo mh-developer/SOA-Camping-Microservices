be3fa92c2f4790215e705f848c0700b49ad16947
Mitja <mh7860@student.uni-lj.si>
2021-01-04 22:04:18 +0100

    fix: add logs formatter for flask api

--- code/ReservationService/resources/logger_provider.py
+++ code/ReservationService/resources/logger_provider.py
@@ -3,6 +3,7 @@ from python_logging_rabbitmq import RabbitMQHandler
 
 logger = logging.getLogger('ReservationService')
 logger.setLevel(logging.INFO)
+formatter = logging.Formatter('''%(asctime)s %(levelname)s %(funcName)s %(module)s Correlation: %(process)d [%(name)s%(pathname)s.%(funcName)s] - %(message)s''')
 logger.addHandler(RabbitMQHandler(
     username="student",
     password="student123",
@@ -10,4 +11,5 @@ logger.addHandler(RabbitMQHandler(
     routing_key_format="SIPIA-4-Logging",
     host="studentdocker.informatika.uni-mb.si",
     port=5672,
+    formatter=formatter,
 ))
